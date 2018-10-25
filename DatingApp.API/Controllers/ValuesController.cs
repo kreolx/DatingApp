4da4509f59aca37c83faa6551cd6c36b879a8f1c
kreolx <kreolx@mail.ru>
2018-10-25 22:33:53 +1000

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            if (value == null)
+                return NoContent();
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult CreateValue([FromBody] string value)
+        {
+            if(!ModelState.IsValid){
+                return Ok(value);
+            }
+            var valModel = new Value(){Name = value};
+            _context.Values.Add(valModel);
+            var id = valModel.Id;
+            _context.SaveChanges();
+            return RedirectToAction("GetValue", "Values", id);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
